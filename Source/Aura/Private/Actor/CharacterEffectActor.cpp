12191e92833c9564c24a0a522f33f22cc95e75a1
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-10-27 13:54:51 +0200

    27. Effect Actor

---
+++ Source/Aura/Private/Actor/CharacterEffectActor.cpp
@@ -0,0 +1,48 @@
+
+
+
+#include "Actor/CharacterEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/ARPGAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+ACharacterEffectActor::ACharacterEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+	
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+
+}
+
+void ACharacterEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// TODO: Change to Gameplay Effect
+	if(IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UARPGAttributeSet* ARPGAttributeSet = Cast<UARPGAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UARPGAttributeSet::StaticClass()));
+		UARPGAttributeSet* MutableARPGAttributeSet = const_cast<UARPGAttributeSet*>(ARPGAttributeSet);
+		MutableARPGAttributeSet->SetHealth(ARPGAttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+}
+
+void ACharacterEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ACharacterEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACharacterEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &ACharacterEffectActor::EndOverlap);
+}
+
---
+++ Source/Aura/Public/Actor/CharacterEffectActor.h
@@ -0,0 +1,44 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CharacterEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AURA_API ACharacterEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ACharacterEffectActor();
+
+	UFUNCTION()
+	virtual void OnOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult & SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
