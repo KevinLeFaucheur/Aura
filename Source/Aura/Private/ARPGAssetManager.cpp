9b78c495612dcf7539af5623b0adeee60d92d1a2
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-11-03 18:54:43 +0100

    118. Receiving Target Data

--- Source/Aura/Private/ARPGAssetManager.cpp
+++ Source/Aura/Private/ARPGAssetManager.cpp
@@ -3,6 +3,8 @@
 
 #include "ARPGAssetManager.h"
 #include "ARPGGameplayTags.h"
+#include "AbilitySystem/ARPGAbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
 
 UARPGAssetManager& UARPGAssetManager::Get()
 {
@@ -16,4 +18,7 @@ void UARPGAssetManager::StartInitialLoading()
 	Super::StartInitialLoading();
 
 	FARPGGameplayTags::InitializeNativeGameplayTags();
+
+	// This is required to use Target Data
+	UAbilitySystemGlobals::Get().InitGlobalData();
 }
