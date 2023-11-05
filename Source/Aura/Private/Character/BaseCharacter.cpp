6904f6aff067f5a0726c88ef5bfac1583ad8002c
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-11-05 18:47:44 +0100

    139. Dissolve Effect

--- Source/Aura/Private/Character/BaseCharacter.cpp
+++ Source/Aura/Private/Character/BaseCharacter.cpp
@@ -45,6 +45,7 @@ void ABaseCharacter::MulticastHandleDeath_Implementation()
 	GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 	
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	Dissolve();
 }
 
 void ABaseCharacter::BeginPlay()
@@ -81,6 +82,22 @@ void ABaseCharacter::AddCharacterAbilities()
 	if(!HasAuthority()) return;
 
 	ARPGASC->AddCharacterAbilities(StartupAbilities);
+}
+
+void ABaseCharacter::Dissolve()
+{
+	if(IsValid(DissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst =  UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+		GetMesh()->SetMaterial(0, DynamicMatInst);
+		StartDissolveTimeline(DynamicMatInst);
+	}
+	if(IsValid(WeaponDissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst =  UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+		Weapon->SetMaterial(0, DynamicMatInst);
+		StartWeaponDissolveTimeline(DynamicMatInst);
+	}
 }
 
 UAbilitySystemComponent* ABaseCharacter::GetAbilitySystemComponent() const
