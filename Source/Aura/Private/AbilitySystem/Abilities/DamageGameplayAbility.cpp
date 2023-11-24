b30388aeca8cc60efda61b90ffc921f795630dce
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-11-24 12:40:16 +0100

    312. Death Impulse Magnitude

--- Source/Aura/Private/AbilitySystem/Abilities/DamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/DamageGameplayAbility.cpp
@@ -38,6 +38,7 @@ FDamageEffectParams UDamageGameplayAbility::MakeDamageEffectParamsFromClassDefau
 	Params.DebuffDamage = DebuffDamage;
 	Params.DebuffFrequency = DebuffFrequency;
 	Params.DebuffDuration = DebuffDuration;
+	Params.DeathImpulseMagnitude = DeathImpulseMagnitude;
 	
 	return Params;
 }
--- Source/Aura/Public/AbilitySystemTypes.h
+++ Source/Aura/Public/AbilitySystemTypes.h
@@ -44,6 +44,9 @@ struct FDamageEffectParams
 	
 	UPROPERTY()
 	float DebuffDuration = 0.f;
+	
+	UPROPERTY()
+	float DeathImpulseMagnitude = 0.f;
 };
 
 USTRUCT(BlueprintType)
